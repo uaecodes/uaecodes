f83fd9d8ac4a94217a1aaeea39e0e071502068e6
rashed <x.suwaidi@gmail.com>
2022-10-27 12:39:51 +0400

    add endpoint

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ root.render(
           <Route path="/" element={<App />} />
           <Route path="/uaecodes" element={<App />} />
           <Route path="/counter" element={<Counter />} />
+          <Route path="/uaecodes/counter" element={<Counter />} />
         </Routes>
       </BrowserRouter>
     </TerminalContextProvider>
